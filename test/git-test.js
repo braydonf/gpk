770c7f9c3ff75e17cddc80937b2b2175a67bb1a5
Braydon Fuller <courier@braydon.com>
2019-12-03 16:23:18 -0800

    lib: refactor and reorganize into package module

---
+++ test/git-test.js
@@ -0,0 +1,203 @@
+/*!
+ * Copyright (c) 2019, Braydon Fuller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const assert = require('assert');
+const {resolve} = require('path');
+
+const {
+  listTags,
+  matchTag,
+  cloneRepo,
+  verifyRepo,
+  archive,
+  listTree,
+  checksum,
+  treeHash,
+  cloneFiles
+} = require('../lib/git');
+
+const {datadir, testdir, testfile} = require('./common');
+
+describe('Git', function() {
+  describe('listTags()', function() {
+    it('should find all tags', async () => {
+      const git = `${datadir}/repo/.git`;
+
+      const tags = await listTags(git);
+      assert.deepEqual(tags, {
+        'v1.0.0': {
+          annotated: 'dff7f77c1a6f17cefec11342a6e410ab83c8488a',
+          commit: '35c9aaaffc27f295981e996a24a0cb9cd7a84ecb'
+        },
+        'v1.1.0': {
+          annotated: '94071e59398891cb2cfd183045237c2494923459',
+          commit: '36ea09f31e77f24a796722adfa51eb62dac043fa'
+        },
+        'v2.0.0': {
+          annotated: '0ab87dcfe1dc8c327b1d8b8e9fa78bb1839ea86f',
+          commit: '294acab444f9742a7c1d0de273b7a412e7809e52'
+        },
+        'v2.1.0': {
+          commit: '3d2115aa2e86d8c08ce1639d177757aa1ce85799'
+        }
+      });
+    });
+  });
+
+  describe('matchTag()', function() {
+    it('should find matching semver tags', async () => {
+      const tags = ['v1.0.0', 'v1.1.0', 'v2.0.0'];
+
+      let tag = null;
+
+      tag = matchTag(tags, '^1.0.0');
+      assert.equal(tag, 'v1.1.0');
+
+      tag = matchTag(tags, '^1.1.0');
+      assert.equal(tag, 'v1.1.0');
+
+      tag = matchTag(tags, '^2.0.0');
+      assert.equal(tag, 'v2.0.0');
+    });
+  });
+
+  describe('cloneRepo()/verifyRepo()', function() {
+    it('should clone and verify signature', async () => {
+      let err = null;
+      const git = `${datadir}/repo/.git`;
+      const tag = 'v1.1.0';
+      const dst = testdir('clone');
+
+      try {
+        await cloneRepo(tag, git, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+
+      try {
+        await verifyRepo(tag, null, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+    });
+
+    it('should clone and verify lightweight tag', async () => {
+      let err = null;
+      const git = `${datadir}/repo/.git`;
+
+      // The tag was created lightweight via `git tag v2.1.0` instead of
+      // `git tag -a v2.1.0 -m "v2.1.0"`. Thus the command `git verify-tag`
+      // can not be used.
+      const tag = 'v2.1.0';
+      const commit = '3d2115aa2e86d8c08ce1639d177757aa1ce85799';
+      const dst = testdir('clonelight');
+
+      try {
+        await cloneRepo(tag, git, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+
+      try {
+        await verifyRepo(null, commit, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+    });
+  });
+
+  describe('archive()/checksum()', function() {
+    it('should archive and checksum repository', async () => {
+      const git = `${datadir}/repo/.git`;
+      let err = null;
+
+      const dst = `${testfile('archive.tar.gz')}`;
+      const expected = 'wNVracPFLWusleCon1AQu+ngVEmjZpliCCgUY2bcfVLr'
+            + 'JCrun9NOVAMM8XgUH3+A4gpbY2JEjmm1+yPii1wiAQ==';
+
+      try {
+        await archive(git, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+
+      let digest = null;
+
+      try {
+        digest = await checksum(dst, 'sha512');
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+      assert(digest);
+      assert.equal(digest.toString('base64'), expected);
+    });
+  });
+
+  describe('treeHash()', function() {
+    it('should compute a sha512 tree of git tree', async () => {
+      const git = `${datadir}/repo/.git`;
+      const base = `${datadir}/repo`;
+
+      let err = null;
+      let digest = null;
+
+      try {
+        digest = await treeHash(git, base, 'sha512');
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+      assert(digest);
+
+      const expected = 'RA5uffYjMoSmx4FmrDpXQUyg3CApDAMCHsFBuANirGx3'
+            + 'fCBz4wN1XQZEzRj6/s20b/gNhAMZVg9f8opnMx//OQ==';
+
+      assert(digest);
+      assert.equal(digest.toString('base64'), expected);
+    });
+  });
+
+  describe('cloneFiles()', function() {
+    it('should clone files to destination', async () => {
+      let err = null;
+      const git = `${datadir}/repo/.git`;
+      const dst = testdir('clonefiles');
+
+      try {
+        await cloneFiles(git, dst);
+      } catch (e) {
+        err = e;
+      }
+
+      assert(!err);
+    });
+  });
+});
---
+++ test/package-test.js
@@ -0,0 +1,274 @@
+/*!
+ * Copyright (c) 2019, Braydon Fuller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const assert = require('assert');
+const util = require('util');
+const fs = require('fs');
+const mkdir = util.promisify(fs.mkdir);
+
+const Package = require('../lib/package');
+const {datadir, testdir, unpack} = require('./common');
+
+describe('Package', function() {
+  describe('resolveRemote()', function() {
+    const hash = '3c0cfdd8445ec81386daa187feb2d32b9f4d89a1';
+
+    const remotes = {
+      local: [
+        `file:${datadir}`
+      ],
+      onion: [
+        'ssh://git@fszyuaceipjhnbyy44mtfmoocwzgzunmdu46votrm5c72poeeffa.onion:22',
+        'ssh://git@xg5jwb4xxwajkhur2ahuhtdwifniyoyvbm5h4yzawawwjziol3jq.onion:22',
+        'ssh://git@23aj5gsggiufl6qhfbmzwd334qyhgaugbh2g3ty4ecl3jikmt5ja.onion:22',
+      ],
+      gitlab: [
+        'https://gitlab.com'
+      ],
+      github: [
+        'https://github.com',
+      ]
+    };
+
+    const vectors = [
+      {
+        input: {
+          name: 'bdb',
+          src: 'github:bcoin-org/bdb#semver:~1.1.7',
+        },
+        output: {
+          git: [
+            'https://github.com/bcoin-org/bdb.git'
+          ],
+          version: '~1.1.7',
+          branch: null,
+        }
+      },
+      {
+        input: {
+          name: 'bdb',
+          src: 'github:bcoin-org/bdb#v1.1.7',
+        },
+        output: {
+          git: [
+            'https://github.com/bcoin-org/bdb.git'
+          ],
+          version: null,
+          branch: 'v1.1.7',
+        }
+      },
+      {
+        input: {
+          name: 'bdb',
+          src: `github:bcoin-org/bdb#${hash}`,
+        },
+        output: {
+          git: [
+            'https://github.com/bcoin-org/bdb.git'
+          ],
+          version: null,
+          branch: `${hash}`,
+        }
+      },
+      {
+        input: {
+          name: 'bdb',
+          src: 'gitlab:bcoin-org/bdb#semver:~1.1.7',
+        },
+        output: {
+          git: [
+            'https://gitlab.com/bcoin-org/bdb.git'
+          ],
+          version: '~1.1.7',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'onion:bcoin/bcoin#semver:~1.1.7'
+        },
+        output: {
+          git: [
+            'ssh://git@fszyuaceipjhnbyy44mtfmoocwzgzunmdu46votrm5c72poeeffa.onion:22/bcoin/bcoin.git',
+            'ssh://git@xg5jwb4xxwajkhur2ahuhtdwifniyoyvbm5h4yzawawwjziol3jq.onion:22/bcoin/bcoin.git',
+            'ssh://git@23aj5gsggiufl6qhfbmzwd334qyhgaugbh2g3ty4ecl3jikmt5ja.onion:22/bcoin/bcoin.git',
+          ],
+          version: '~1.1.7',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'repo',
+          src: 'local:repo#semver:~1.1.7'
+        },
+        output: {
+          git: [
+            `${datadir}/repo/.git`
+          ],
+          version: '~1.1.7',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'repo',
+          src: 'local:#semver:~1.1.7'
+        },
+        output: {
+          git: [
+            `${datadir}/repo/.git`
+          ],
+          version: '~1.1.7',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'git+https://github.com/bcoin-org/bcfg.git#semver:~2.0.0'
+        },
+        output: {
+          git: ['https://github.com/bcoin-org/bcfg.git'],
+          version: '~2.0.0',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'git+ssh://git@github.com/bcoin-org/bcoin.git#semver:~2.0.0'
+        },
+        output: {
+          git: ['ssh://git@github.com/bcoin-org/bcoin.git'],
+          version: '~2.0.0',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'git+https://github.com/bcoin-org/bcfg.git#v2.0.0'
+        },
+        output: {
+          git: ['https://github.com/bcoin-org/bcfg.git'],
+          version: null,
+          branch: 'v2.0.0'
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: `git+ssh://git@github.com/bcoin-org/bcoin.git#${hash}`
+        },
+        output: {
+          git: ['ssh://git@github.com/bcoin-org/bcoin.git'],
+          version: null,
+          branch: `${hash}`
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'git://github.com/bcoin-org/bcoin.git'
+        },
+        output: {
+          git: ['git://github.com/bcoin-org/bcoin.git'],
+          version: null,
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'bcoin',
+          src: 'git://github.com/bcoin-org/bcoin.git#semver:~2.0.0'
+        },
+        output: {
+          git: ['git://github.com/bcoin-org/bcoin.git'],
+          version: '~2.0.0',
+          branch: null
+        }
+      },
+      {
+        input: {
+          name: 'repo',
+          src: '~1.1.7'
+        },
+        output: {
+          git: [],
+          version: '~1.1.7',
+          branch: null
+        }
+      }
+    ];
+
+    for (const {input, output} of vectors) {
+      it(`${input.src}`, () => {
+        const mod = new Package({
+          datadir,
+          info: {
+            remotes: remotes,
+            dependencies: {}
+          }
+        });
+
+        mod.info.dependencies[input.name] = input.src;
+
+        const remote = mod.resolveRemote(input.name);
+        assert.deepEqual(remote, output);
+      });
+    }
+  });
+
+  describe('fromDirectory()', function() {
+    it('should locate and read package.json', async () => {
+      let err = null;
+
+      const mod = await Package.fromDirectory(`${datadir}/modules/foo/lib`);
+
+      assert.equal(mod.dir, `${datadir}/modules/foo`);
+      assert.deepEqual(mod.info, {
+        name: 'foo',
+        version: '1.0.0',
+        main: './lib/index.js',
+        remotes: {
+          local: ['file:../'],
+        },
+        dependencies: {
+          bar: 'local:bar#semver:^1.0.0',
+          beep: 'local:beep#semver:^1.0.0',
+          bloop: 'local:bloop#semver:^1.0.0'
+        }
+      });
+    });
+  });
+
+  describe('install()', function() {
+    it('should install dependencies', async () => {
+      // Setup the test modules.
+      const modules = testdir('install');
+      await mkdir(modules);
+      await unpack(`${datadir}/modules.tar.gz`, modules);
+
+      // Install the dependencies of foo module.
+      const mod = await Package.fromDirectory(`${modules}/modules/foo`);
+      await mod.install();
+    });
+  });
+});
